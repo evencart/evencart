c66246875f0e94c8fccf9189dd1d2a45bb05bf38
anshul@apexol.com <anshul@apexol.com>
2019-04-03 13:10:32 +0530

    serialization use single instance of settings

--- RoastedMarketplace.Services/Serializers/JsonDataSerializer.cs
+++ RoastedMarketplace.Services/Serializers/JsonDataSerializer.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 
 namespace RoastedMarketplace.Services.Serializers
 {
     public class JsonDataSerializer : IDataSerializer
     {
+        private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Converters = new List<JsonConverter>()
+            {
+                new StringEnumConverter()
+            }
+        };
+
         public string Serialize(object obj, bool camelCase = true)
         {
-            return JsonConvert.SerializeObject(obj, Formatting.None, new JsonSerializerSettings()
-            {
-                ContractResolver = camelCase ? new CamelCasePropertyNamesContractResolver() : new DefaultContractResolver()
-            });
+            var currentResolver = SerializerSettings.ContractResolver;
+            if (!camelCase)
+                SerializerSettings.ContractResolver = new DefaultContractResolver();
+            var serializedData = JsonConvert.SerializeObject(obj, Formatting.None, SerializerSettings);
+            //reset the settings
+            SerializerSettings.ContractResolver = currentResolver;
+            return serializedData;
         }
 
         public T DeserializeAs<T>(string serializedData)
