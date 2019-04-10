da070137374e90f23ce262bdff4452f8a06c2987
anshul@apexol.com <anshul@apexol.com>
2019-04-10 14:52:36 +0530

    Modified slug generator to a better algo

--- RoastedMarketplace.Core/CommonHelper.cs
+++ RoastedMarketplace.Core/CommonHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace RoastedMarketplace.Core
 {
@@ -15,7 +17,7 @@ namespace RoastedMarketplace.Core
                 return "";
 
             var s = RemoveAccent(phrase).ToLower();
-            s = Regex.Replace(s, @"[^a-z0-9\s-]", "");                      // remove invalid characters
+            s = Regex.Replace(s, @"[^a-z0-9\s-_]", "");                      // remove invalid characters
             s = Regex.Replace(s, @"\s+", " ").Trim();                       // single space
             s = s.Substring(0, s.Length <= 45 ? s.Length : 45).Trim();      // cut and trim
             s = Regex.Replace(s, @"\s", "-");                               // insert hyphens
@@ -24,8 +26,19 @@ namespace RoastedMarketplace.Core
 
         public static string RemoveAccent(string txt)
         {
-            var bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
-            return System.Text.Encoding.ASCII.GetString(bytes);
+            var normalizedString = txt.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
     }
 }
