ce0ead129e4b0420d9f262012f44f093b344e2ea
anshul@apexol.com <anshul@apexol.com>
2019-05-06 15:41:59 +0530

    Remove unused plugin

--- src/Plugins/Payments.PaypalDirect/PaypalDirectConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Payments.PaypalDirect
-{
-    public static class PaypalDirectConfig
-    {
-        public const string PaymentHandlerComponentRouteName = "PaypalDirectPaymentHandler";
-    }
-}
