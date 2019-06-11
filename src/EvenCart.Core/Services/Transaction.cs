a1ea783f1194b1bdcc112b6aae59df12953d7cb5
anshul@apexol.com <anshul@apexol.com>
2019-06-11 15:44:46 +0530

    Allow cancellation of transaction and return the status

--- src/EvenCart.Core/Services/Transaction.cs
+++ src/EvenCart.Core/Services/Transaction.cs
@@ -7,13 +7,25 @@ namespace EvenCart.Core.Services
     {
         internal IDotEntityTransaction Value { get; set; }
 
-        public static void Initiate(Action<Transaction> operationAction)
+        private bool RevertTransaction { get; set; }
+
+        public void Fail()
+        {
+            RevertTransaction = true;
+        }
+        public static bool Initiate(Action<Transaction> operationAction)
         {
             using (var t = EntitySet.BeginTransaction())
             {
                 var transaction = new Transaction() { Value = t };
                 operationAction(transaction);
+                if (transaction.RevertTransaction)
+                {
+                    //don't do anything because transaction was stopped
+                    return false;
+                }
                 t.Commit();
+                return t.Success;
             }
         }
     }
