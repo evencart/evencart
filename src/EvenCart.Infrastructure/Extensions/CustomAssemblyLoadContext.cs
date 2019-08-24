8b22ddc5a5a6d09a5b440fd7fef488adf3e05d77
anshul@apexol.com <anshul@apexol.com>
2019-08-24 15:46:13 +0530

    SYNC: Missed file from previous commit
    (cherry picked from commit 5608c729a33c9c6b516171446703c2a70b9240bc)

---
+++ src/EvenCart.Infrastructure/Extensions/CustomAssemblyLoadContext.cs
@@ -0,0 +1,24 @@
+﻿namespace EvenCart.Infrastructure.Extensions
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.Loader;
+
+    internal class CustomAssemblyLoadContext : AssemblyLoadContext
+    {
+        public IntPtr LoadUnmanagedLibrary(string absolutePath)
+        {
+            return this.LoadUnmanagedDll(absolutePath);
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            return this.LoadUnmanagedDllFromPath(unmanagedDllName);
+        }
+    }
+}
