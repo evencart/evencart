8c216f3ff68d6200d243f73b70c006bf95773e10
anshul@apexol.com <anshul@apexol.com>
2020-01-27 18:47:09 +0530

    Fix user subscription retrieval and added tests
    (cherry picked from commit b39bf329cc30deb2fade2f6c83bb7e68c46e631c)

---
+++ src/Tests/EvenCart.Services.Tests/MySql/MySqlUserSubscriptionTests.cs
@@ -0,0 +1,14 @@
+﻿using EvenCart.Services.Tests.Users;
+using NUnit.Framework;
+
+namespace EvenCart.Services.Tests.MySql
+{
+    [TestFixture]
+    public class MySqlUserSubscriptionTests : UserSubscriptionTests
+    {
+        public MySqlUserSubscriptionTests()
+        {
+            TestDbInit.MySql(MySqlConnectionString);
+        }
+    }
+}
