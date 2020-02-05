534a7c77a1a349667cef06796362d4f3e357beeb
anshul@apexol.com <anshul@apexol.com>
2020-02-05 17:08:55 +0530

    Fix conflicting product check

--- src/EvenCart.Services/Helpers/CartHelper.cs
+++ src/EvenCart.Services/Helpers/CartHelper.cs
@@ -33,11 +33,7 @@ namespace EvenCart.Services.Helpers
         public static bool HasConflictingProducts(Cart cart)
         {
             return cart.CartItems.Count > 1 &&
-                   (cart.CartItems.All(x => x.Product.ProductSaleType != ProductSaleType.OneTime) ||
-                    cart.CartItems.All(x =>
-                        x.Product.ProductSaleType != ProductSaleType.Subscription) ||
-                    cart.CartItems.All(x =>
-                        x.Product.ProductSaleType != ProductSaleType.Both));
+                   cart.CartItems.Select(x => x.Product.ProductSaleType).Distinct().Count() > 1;
         }
 
         public static bool IsSubscriptionCart(Cart cart)
