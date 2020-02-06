8d1402ff3cfbbdccaa6edfed94dea0bc0ae2fb14
anshul@apexol.com <anshul@apexol.com>
2020-02-06 20:37:24 +0530

    log the keep alive requests

--- src/EvenCart.Infrastructure/Tasks/KeepAliveTask.cs
+++ src/EvenCart.Infrastructure/Tasks/KeepAliveTask.cs
@@ -1,21 +1,25 @@
 ﻿using System.Threading.Tasks;
 using EvenCart.Core.Tasks;
 using EvenCart.Infrastructure.Routing;
+using EvenCart.Services.Extensions;
 using EvenCart.Services.HttpServices;
+using EvenCart.Services.Logger;
 
 namespace EvenCart.Infrastructure.Tasks
 {
     public class KeepAliveTask : ITask
     {
         private readonly IRequestProvider _requestProvider;
-
-        public KeepAliveTask(IRequestProvider requestProvider)
+        private readonly ILogger _logger;
+        public KeepAliveTask(IRequestProvider requestProvider, ILogger logger)
         {
             _requestProvider = requestProvider;
+            _logger = logger;
         }
 
         public async Task Run()
         {
+            _logger.LogInfo<KeepAliveTask>(null, "Starting a keep alive request");
             await _requestProvider.GetStringAsync(ApplicationEngine.RouteUrl(RouteNames.KeepAlive, null, true));
         }
 
--- src/EvenCart/Controllers/KeepAliveController.cs
+++ src/EvenCart/Controllers/KeepAliveController.cs
@@ -1,14 +1,24 @@
 ﻿using EvenCart.Infrastructure.Mvc;
 using EvenCart.Infrastructure.Routing;
+using EvenCart.Services.Extensions;
+using EvenCart.Services.Logger;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EvenCart.Controllers
 {
     public class KeepAliveController : FoundationController
     {
+        private readonly ILogger _logger;
+
+        public KeepAliveController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         [Route("~/keep-alive", Name = RouteNames.KeepAlive)]
         public IActionResult Index()
         {
+            _logger.LogInfo<KeepAliveController>(null, "Keep alive request received");
             return Content("");
         }
     }
