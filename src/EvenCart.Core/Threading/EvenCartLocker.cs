88f712dcee58c801632e9479dd29d6b018cf8188
anshul@apexol.com <anshul@apexol.com>
2020-02-22 16:16:39 +0530

    avoid script minification for textarea and pre tags

---
+++ src/EvenCart.Core/Threading/EvenCartLocker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace EvenCart.Core.Threading
+{
+    public class EvenCartLocker : IDisposable
+    {
+        private readonly ReaderWriterLockSlim _lock;
+
+        public EvenCartLocker(ReaderWriterLockSlim @lock)
+        {
+            _lock = @lock;
+            _lock.EnterWriteLock();
+        }
+
+        public void Dispose()
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+}
