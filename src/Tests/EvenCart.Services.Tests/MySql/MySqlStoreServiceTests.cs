e3b5a75a621a9b657aff83c5e35409e88d6839ef
anshul@apexol.com <anshul@apexol.com>
2020-03-02 15:57:13 +0530

    Store cloning tests added

---
+++ src/Tests/EvenCart.Services.Tests/MySql/MySqlStoreServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using EvenCart.Services.Tests.Stores;
+using NUnit.Framework;
+
+namespace EvenCart.Services.Tests.MySql
+{
+    [TestFixture]
+    public class MySqlStoreServiceTests : StoreServiceTests
+    {
+        public MySqlStoreServiceTests()
+        {
+            TestDbInit.MySql(MySqlConnectionString);
+        }
+    }
+}
