9f25cbc9ffac735a594f327ef00fdeac765a3ff5
anshul@apexol.com <anshul@apexol.com>
2020-03-06 15:59:34 +0530

    update code with license header

--- src/EvenCart.Services/Tokens/Token.cs
+++ src/EvenCart.Services/Tokens/Token.cs
@@ -1,4 +1,15 @@
-﻿namespace EvenCart.Services.Tokens
+﻿#region License
+// Copyright (c) Sojatia Infocrafts Private Limited.
+// The following code is part of EvenCart eCommerce Software (https://evencart.co) Dual Licensed under the terms of
+// 
+// 1. GNU GPLv3 with additional terms (available to read at https://evencart.co/license)
+// 2. EvenCart Proprietary License (available to read at https://evencart.co/license/commercial-license).
+// 
+// You can select one of the above two licenses according to your requirements. The usage of this code is
+// subject to the terms of the license chosen by you.
+#endregion
+
+namespace EvenCart.Services.Tokens
 {
     public class Token
     {
--- src/EvenCart/Factories/Orders/OrderModelFactory.cs
+++ src/EvenCart/Factories/Orders/OrderModelFactory.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections.Generic;
+﻿#region License
+// Copyright (c) Sojatia Infocrafts Private Limited.
+// The following code is part of EvenCart eCommerce Software (https://evencart.co) Dual Licensed under the terms of
+// 
+// 1. GNU GPLv3 with additional terms (available to read at https://evencart.co/license)
+// 2. EvenCart Proprietary License (available to read at https://evencart.co/license/commercial-license).
+// 
+// You can select one of the above two licenses according to your requirements. The usage of this code is
+// subject to the terms of the license chosen by you.
+#endregion
+
+using System.Collections.Generic;
 using System.Linq;
 using EvenCart.Data.Entity.Addresses;
 using EvenCart.Data.Entity.Purchases;
